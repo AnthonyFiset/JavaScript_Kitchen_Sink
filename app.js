9141834804eeaee8f981f7f44ab135237db4004e
Anthony Fiset <anthonyfiset@Anthonys-iMac.phub.net.cable.rogers.com>
2021-12-28 18:14:00 -0500

    Working on the JAVA file, added some variables & constants

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-var name = Anthony
+var name = "Anthony"
+const states = 50
+// When using the variable math it will automatically give the result of adding 5 + 4!!!
+var math = 5 + 4
+
+console.log(math)
