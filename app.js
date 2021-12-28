784ec2acd1fd81da01eabde9a46d73a895f008b8
Anthony Fiset <anthonyfiset@Anthonys-iMac.phub.net.cable.rogers.com>
2021-12-28 18:14:49 -0500

    More JS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var name = "Anthony"
 const states = 50
-// When using the variable math it will automatically give the result of adding 5 + 4!!!
+// When using the variable math it will automatically give the result of adding 5 + 4!!!    
 var math = 5 + 4
 
 console.log(math)
