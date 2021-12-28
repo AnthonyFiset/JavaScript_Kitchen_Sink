074f2e8475622fb5a15dc390466c763ad3661099
Anthony Fiset <anthonyfiset@Anthonys-iMac.phub.net.cable.rogers.com>
2021-12-28 18:54:19 -0500

    Added first function, now working on second one

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+// More Variables
 var name = "Anthony"
 const states = 50
 // When using the variable math it will automatically give the result of adding 5 + 4!!!    
 var math = 5 + 4
 
-console.log(math)
+// Attempting a function
+
+function sayHello() {
+    alert("Hello World!");
+}
+
+sayHello();
+
+// Second Function
+
+function checkAge() {
+    
+}
