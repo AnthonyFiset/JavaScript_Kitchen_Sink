c016c3b1e3892ef20282a63e702f29639cb3f089
Anthony Fiset <anthonyfiset@Anthonys-iMac.phub.net.cable.rogers.com>
2021-12-28 21:25:53 -0500

    Almost done with this project

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // More Variables
-var name = "Anthony"
-const states = 50
+var person = "Anthony";
+const states = 50;
 // When using the variable math it will automatically give the result of adding 5 + 4!!!    
-var math = 5 + 4
+var math = 5 + 4;
 
 // Attempting a function
 
@@ -14,6 +14,35 @@ sayHello();
 
 // Second Function
 
-function checkAge() {
-    
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
+    }
 }
+
+let charles = {
+    name: "Charles",
+    age: 21
+};
+
+let abby = {
+    name: "Abby",
+    age: 27
+};
+
+let james = {
+    name: "James",
+    age: 18
+};
+
+let john = {
+    name: "John",
+    age: 17
+};
+
+checkAge(charles.name, charles.age);
+checkAge(abby.name, abby.age);
+checkAge(james.name, james.age);
+checkAge(john.name, john.age);
+
+// 
